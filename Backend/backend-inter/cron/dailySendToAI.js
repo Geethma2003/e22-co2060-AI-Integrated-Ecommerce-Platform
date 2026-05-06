12de855cef88245894fca9a325254c4d303f9aa2
tharu200304 <e22154@eng.pdn.ac.lk>
2026-05-06 09:53:27 +0530

    Merge branch 'main' of https://github.com/cepdnaclk/e22-co2060-AI-Integrated-Ecommerce-Platform

--- Backend/backend-inter/cron/dailySendToAI.js
+++
@@ -1,8 +0,0 @@
-import cron from "node-cron";
-import { sendProductsToAI } from "../services/aiService.js";
-
-// Every day at 2 AM
-cron.schedule("0 2 * * *", async () => {
-  console.log("⏰ Daily AI cron started");
-  await sendProductsToAI();
-});
